c53ea874df97c11f043423b2128efdae732335e6
joshuadebellis <joshua.debellis@gmail.com>
2014-05-27 21:53:28 +0100

    Update roulette.py

--- roulette.py
+++ roulette.py
@@ -1,7 +1,7 @@
 import random
 
 class Roulette:
-    play_again = True
+
     def __init__(self, bankroll):
         self.bankroll = bankroll
     def wheelspin(self, bet, bet_2):
@@ -14,29 +14,24 @@ class Roulette:
              self.bankroll -= bet
              return False
         
-# initialize variables
-play_again = True
-bet = 0
-bet2 = ''
-pot = 0
-bankroll = 0
-
-bankroll = int(raw_input("What's your starting pot ? "))
-gambling = Roulette(bankroll)
-
-while play_again == True:
+def play_game():
     bet = int(raw_input("How much would you like to bet? "))
-
-    valid_bet = False
-    while valid_bet != True:
-        bet_2 = raw_input("Red or black ? ").lower()
-        valid_bet = (bet_2 in ('red', 'black'))
+    bet_2 = raw_input("Red or black ? ").lower()
     winner_is_you = gambling.wheelspin(bet, bet_2)
     if winner_is_you:
         print "You win!"
+        print "your current bankroll is " , gambling.bankroll
     else:
-        print "You lose!"
-    print "your current bankroll is " , gambling.bankroll
-    question = raw_input("Would you like to play again? ")
-    play_again = question == "yes"
+       print "You lose!"
+       print "your current bankroll is " , gambling.bankroll
+       
+    play_again == raw_input("Would you like to play again? ")
+    return play_again
+
+    
+play_again = True
+bankroll = int(raw_input("What's your starting pot ? "))
+gambling = Roulette(bankroll)
+if play_again:
+    play_game()
     
